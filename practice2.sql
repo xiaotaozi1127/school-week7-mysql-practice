42f70bcd5039c96fa88e4fc6515cdaa4385310d3
htao <htao@thoughtworks.com>
2018-04-17 11:24:50 +0800

    [taohui] get top salary employee for each company

--- practice2.sql
+++ practice2.sql
@@ -1,2 +1,9 @@
 # 取得每个company中最高薪水的人员名字
+select company.companyName as company_name, employee.name as employee_name
+from company JOIN employee
+on employee.id =
+   (SELECT id FROM employee
+   where employee.companyId = company.id
+    ORDER BY salary desc
+    limit 1);
 # 输出结果包含公司名称和人员名称：companyName name
