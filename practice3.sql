7ca5ae4b2fa513db2c8c5c7dc7f4e355966b5bea
htao <htao@thoughtworks.com>
2018-04-17 11:40:02 +0800

    [taohui] get company name with highest avg salary

--- practice3.sql
+++ practice3.sql
@@ -1,4 +1,9 @@
 #取得平均薪水最高的公司
+SELECT companyName, 
+(SELECT avg(salary) from employee 
+where companyId = company.id) as avgSalary from company
+ORDER BY avgSalary DESC
+limit 1;
 #输出公司名称和平均薪水：companyName avgSalary
 
 
