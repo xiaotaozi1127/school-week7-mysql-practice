9faac3426dea69934dbefa936251fe739533d7e3
htao <htao@thoughtworks.com>
2018-04-17 13:10:31 +0800

    [taohui] get employees whose salary is higher than average salary

--- practice4.sql
+++ practice4.sql
@@ -1,3 +1,18 @@
 # 找出薪水在公司的平均薪水之上的人员名字
 # 输出标准中包含Employee中所有字段和该employee所在公司名称以及该公司平均薪资：
 # id | name | age | gender | companyId | salary | companyName | avgsal
+
+CREATE TEMPORARY TABLE companyAvgSalary(companyId smallint, companyName VARCHAR(20), avgSal SMALLINT);
+
+INSERT into companyAvgSalary
+  (
+SELECT id, companyName,
+  (SELECT avg(salary) from employee where companyId = company.id)
+from company);
+
+SELECT id, name, age, gender, employee.companyId,
+  salary, companyName, avgSal from employee, companyAvgSalary
+where employee.companyId = companyAvgSalary.companyId
+and employee.salary >= companyAvgSalary.avgSal;
+
+DROP table companyAvgSalary;
